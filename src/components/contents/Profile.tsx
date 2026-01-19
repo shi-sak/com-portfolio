f0fa56f75558494bce4c5d5c92bc237ab1f246ad
shi-sak <tanaka.kakuei.no.1@gmail.com>
2026-01-19 18:22:16 +0900

    profile

--- src/components/contents/Profile.tsx
+++ src/components/contents/Profile.tsx
@@ -1,20 +1,28 @@
+import icon from "../../assets/icon.jpeg";
+
 export const Profile = () => {
     return (
         <div className="space-y-6">
             <div className="flex gap-4 items-end border-b border-gray-200 pb-4">
-                <div className="text-4xl">🍙</div>
+                <div className="flex">
+                    <img 
+                    className="w-16 aspect-square object-cover"
+                    src={icon}
+                    />
+
+                </div>
                 <div>
-                    <h3 className="text-lg font-bold">仕込む / shicom</h3>
-                    <p className="text-sm text-gray-400">なにここ</p>
+                    <h3 className="text-lg font-bold">仕込む</h3>
+                    <p className="text-sm text-gray-400">shicom</p>
                 </div>
             </div>
             <p>
-                あ<br />
-                なるほど
+                絵や漫画を描きます<br />
+                モノクロの絵が好きです
             </p>
             <div className="bg-gray-50 p-4 rounded-xl text-sm">
-                <h4 className="font-bold mb-2">Love</h4>
-                <p>なんでも！</p>
+                <h4 className="font-bold mb-2">👉参加予定イベント</h4>
+                <p>2026/2/22 COMITIA155</p>
             </div>
         </div>
     );
